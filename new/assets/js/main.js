3f10221550e446b302ce945a0c4f71ca13da6317
sergiuoctavian <octavian.sergiu@gmail.com>
2017-08-28 01:07:55 +0300

    updates

--- new/assets/js/main.js
+++ new/assets/js/main.js
@@ -30,9 +30,36 @@ function lazyLoad(){
   // });  
 }
 
+function scrollIndicator(){
+  var $w = $(window);
+  var $circ = $('.animated-circle');
+  var $progCount = $('.progress-count');
+  var $prog2 = $('.progress-indicator-2');
+  var wh = $w.height();
+  var h = $('html').height();
+  var sHeight = h - wh;
+  $w.on('scroll', function () {
+      var perc = Math.max(0, Math.min(1, $w.scrollTop() / sHeight));
+      updateProgress(perc);
+  });
+
+  function updateProgress(perc) {
+      var circle_offset = 126 * perc;
+      $circ.css({
+          "stroke-dashoffset": 126 - circle_offset
+      });
+      $progCount.html(Math.round(perc * 100) + "%");
+
+      $prog2.css({
+          width: perc * 100 + '%'
+      });
+  }
+}
+
 // init carousel
 $(document).on('ready', function() {
   lazyLoad();
+  scrollIndicator();
   // sleek();
 });
 
@@ -69,6 +96,7 @@ $(function() {
             onAfter: function($container, $newContent) {
               //reinit
               lazyLoad();
+              scrollIndicator();
               // sleek();
               
               // redirectSlider();
@@ -89,31 +117,6 @@ $(function() {
 // }
 
 
- (function () {
-    var $w = $(window);
-    var $circ = $('.animated-circle');
-    var $progCount = $('.progress-count');
-    var $prog2 = $('.progress-indicator-2');
-    var wh = $w.height();
-    var h = $('html').height();
-    var sHeight = h - wh;
-    $w.on('scroll', function () {
-        var perc = Math.max(0, Math.min(1, $w.scrollTop() / sHeight));
-        updateProgress(perc);
-    });
 
-    function updateProgress(perc) {
-        var circle_offset = 126 * perc;
-        $circ.css({
-            "stroke-dashoffset": 126 - circle_offset
-        });
-        $progCount.html(Math.round(perc * 100) + "%");
-
-        $prog2.css({
-            width: perc * 100 + '%'
-        });
-    }
-
-}());
 
 
